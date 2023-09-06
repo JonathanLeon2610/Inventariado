c60c793dc3cd1c8397a8f3d96fcacd77b338374a
jonathanleon2610 <jonathan2610leon@gmail.com>
2023-09-06 12:18:00 -0700

    Terminar eliminacion de imagenes

--- src/pages/EditBienInventarible.jsx
+++ src/pages/EditBienInventarible.jsx
@@ -1,7 +1,13 @@
-import { useEffect, useState,useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import Swal from "sweetalert2";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faArrowLeft,faUpload, faCheck, faBan} from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faUpload,
+  faCheck,
+  faBan,
+} from "@fortawesome/free-solid-svg-icons";
+
 function EditBienInventariable() {
   const [inputValues, setInputValues] = useState({
     id: "",
@@ -20,9 +26,14 @@ function EditBienInventariable() {
 
   const [data, setData] = useState(null);
   const [marcas, setMarcas] = useState([]);
+  // eslint-disable-next-line no-unused-vars
   const [selectedMarca, setSelectedMarca] = useState(""); // Estado para la marca seleccionada
   const [selectedImages, setSelectedImages] = useState([]); // Estado para la imagen seleccionada
-
+  const [imagesId, setImagesId] = useState([]); // Estado para las imagenes seleccionadas
+  const [loadImage, setLoadImage] = useState(null); // Estado para las imagenes seleccionadas
+  const url = window.location.pathname;
+  const segments = url.split("/");
+  const ultimoValor = segments[segments.length - 1];
 
   const fileInputRef = useRef(null); // Ref para el input de archivo oculto
   const handleShowFileInput = () => {
@@ -35,14 +46,41 @@ function EditBienInventariable() {
       const newImages = Array.from(files).map((file) =>
         URL.createObjectURL(file)
       );
-      setSelectedImages((prevImages) => [...prevImages, ...newImages]);
+      setLoadImage(newImages);
     }
   };
 
-  const handleRemoveImage = (index) => {
+  const handleRemoveImage = (index, id) => {
     const newImages = [...selectedImages];
-    newImages.splice(index, 1);
-    setSelectedImages(newImages);
+    console.log(
+      `https://192.168.10.100/api/v1/ActivoBien/${ultimoValor}/doctos/del/${id}`
+    );
+    var myHeaders = new Headers();
+    myHeaders.append(
+      "Authorization",
+      "Bearer " + localStorage.getItem("token")
+    );
+
+    var raw = "";
+
+    var requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://192.168.10.100/api/v1/ActivoBien/${ultimoValor}/doctos/del/${id}`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        console.log(result);
+        newImages.splice(index, 1);
+          setSelectedImages(newImages);
+      })
+      .catch((error) => console.log("error", error));
   };
 
   useEffect(() => {
@@ -76,9 +114,7 @@ function EditBienInventariable() {
           modelo: result.modelo,
         });
         setData(result);
-        console.log(result);
         setSelectedMarca(result.marca);
-        console.log(inputValues);
       })
       .catch(() => {
         Swal.fire({
@@ -92,10 +128,6 @@ function EditBienInventariable() {
   }, []);
 
   useEffect(() => {
-    const url = window.location.pathname;
-    const segments = url.split("/");
-    const ultimoValor = segments[segments.length - 1];
-
     var myHeaders = new Headers();
     myHeaders.append(
       "Authorization",
@@ -114,11 +146,16 @@ function EditBienInventariable() {
     )
       .then((response) => response.json())
       .then((result) => {
+        const imageUrls = result.map(
+          (item) => "https://192.168.10.100/" + item.fileUrl
+        );
+        setSelectedImages(imageUrls);
+        setImagesId(result.map((item) => item.id));
         console.log(result);
       })
       .catch(() => {
-
-        });
+        // Manejo de errores
+      });
   }, []);
 
   useEffect(() => {
@@ -157,8 +194,8 @@ function EditBienInventariable() {
     )
       .then((response) => response.json())
       .then((result) => {
-        // Realiza cualquier acción adicional necesaria después de la actualización
         console.log(result);
+        // Realiza cualquier acción adicional necesaria después de la actualización
       })
       .catch((error) => {
         console.log(error);
@@ -171,7 +208,13 @@ function EditBienInventariable() {
         <>
           <div>
             <h2 style={{ marginLeft: "1rem" }}>Editar Bien Inventariable</h2>
-            <button className="add" style={{marginLeft:"1rem", backgroundColor:"gray"}} onClick={() => (window.location.href = "/main")}><FontAwesomeIcon icon={faArrowLeft}/> Regresar</button>
+            <button
+              className="add"
+              style={{ marginLeft: "1rem", backgroundColor: "gray" }}
+              onClick={() => (window.location.href = "/main")}
+            >
+              <FontAwesomeIcon icon={faArrowLeft} /> Regresar
+            </button>
             <div className="formulario-container">
               <input
                 type="text"
@@ -210,8 +253,7 @@ function EditBienInventariable() {
                 }
               >
                 {marcas.map((marca) => (
-                  <option key={marca.id} value={marca.id}
-                  >
+                  <option key={marca.id} value={marca.id}>
                     {marca.name}
                   </option>
                 ))}
@@ -252,7 +294,9 @@ function EditBienInventariable() {
                   setInputValues({ ...inputValues, TipoAlta: e.target.value })
                 }
               >
-                <option value={data.tipoAlta} disabled>{data.tipoAlta}</option>
+                <option value={data.tipoAlta} disabled>
+                  {data.tipoAlta}
+                </option>
                 <option value={"compra"}>Compra</option>
                 <option value={"Dato"}>Como Dato</option>
                 <option value={"donacion"}>Donaciòn</option>
@@ -266,49 +310,116 @@ function EditBienInventariable() {
                   setInputValues({ ...inputValues, costo: e.target.value })
                 }
               />
-              <button onClick={handleEdit}> <FontAwesomeIcon icon={faCheck}/> Confirmar cambios</button>
+              <button onClick={handleEdit}>
+                {" "}
+                <FontAwesomeIcon icon={faCheck} /> Confirmar cambios
+              </button>
               <button
                 style={{ backgroundColor: "red" }}
-                onClick={() => Swal.fire({
-                  title: "¡Estas Seguro?",
-                  text: "Se perderan los cambios realizados",
-                  icon: "warning",
-                  showCancelButton: true,
-                  confirmButtonColor: "#3085d6",
-                  cancelButtonColor: "#d33",
-                  confirmButtonText: "Si, Salir",
-                  cancelButtonText: "No, Cancelar",
-                }).then((result) => {
-                  if (result.isConfirmed) {
-                    window.location.href = "/main";
-                  }
-                })}
+                onClick={() =>
+                  Swal.fire({
+                    title: "¡Estas Seguro?",
+                    text: "Se perderan los cambios realizados",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Si, Salir",
+                    cancelButtonText: "No, Cancelar",
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      window.location.href = "/main";
+                    }
+                  })
+                }
               >
-                <FontAwesomeIcon icon={faBan}/> Cancelar
+                <FontAwesomeIcon icon={faBan} /> Cancelar
               </button>
               <div className="images-area-container">
+                {selectedImages.length > 0 ? (
+                  <>
+                    <h2>Imagenes disponibles</h2>
+                    <div className="images-area">
+                      {selectedImages.map((imageSrc, index) => (
+                        <>
+                          <div key={index} className="image-preview">
+                            <span
+                              className="remove-image"
+                              onClick={() =>
+                                Swal.fire({
+                                  title:
+                                    "¿Estas Seguro de eliminar esta imagen?",
+                                  text: "Esta accion no se puede revertir",
+                                  icon: "warning",
+                                  showCancelButton: true,
+                                  confirmButtonColor: "#3085d6",
+                                  cancelButtonColor: "#d33",
+                                  confirmButtonText: "Si, Eliminar",
+                                  cancelButtonText: "No, Cancelar",
+                                }).then((result) => {
+                                  if (result.isConfirmed) {
+                                    handleRemoveImage(index, imagesId[index]);
+                                    console.log(imagesId[index]);
+                                  }
+                                })
+                              }
+                            >
+                              &#10005;
+                            </span>
+                            <img
+                              src={imageSrc}
+                              alt={`Previsualización de imagen ${index + 1}`}
+                            />
+                          </div>
+                        </>
+                      ))}
+                    </div>
+                  </>
+                ) : (
+                  <h1>No hay imagenes disponibles</h1>
+                )}
+              </div>
+
+              <div className="images-area-container">
                 <div className="images-area">
-                {selectedImages.map((imageSrc, index) => (
-                    <div key={index} className="image-preview">
+                  {loadImage ? (
+                    <div className="image-preview">
                       <span
                         className="remove-image"
-                        onClick={() => handleRemoveImage(index)}
+                        onClick={() =>
+                          Swal.fire({
+                            title: "¿Estas Seguro de eliminar esta imagen?",
+                            text: "Esta accion no se puede revertir",
+                            icon: "warning",
+                            showCancelButton: true,
+                            confirmButtonColor: "#3085d6",
+                            cancelButtonColor: "#d33",
+                            confirmButtonText: "Si, Eliminar",
+                            cancelButtonText: "No, Cancelar",
+                          }).then((result) => {
+                            if (result.isConfirmed) {
+                              setLoadImage(null);
+                            }
+                          })
+                        }
                       >
                         &#10005;
                       </span>
-                      <img src={imageSrc} alt={`Previsualización de imagen ${index + 1}`} />
+                      <img src={loadImage} alt={`Previsualización de imagen`} />
                     </div>
-                  ))}
-
+                  ) : (
+                    ""
+                  )}
                 </div>
-                <button onClick={handleShowFileInput}><FontAwesomeIcon icon={faUpload}/> Subir nueva Imagen</button>
+                <button onClick={handleShowFileInput}>
+                  <FontAwesomeIcon icon={faUpload} /> Subir nueva Imagen
+                </button>
                 <input
                   type="file"
-                  accept="image/*" // Puedes ajustar los tipos de archivos que acepta
+                  accept="image/*"
                   style={{ display: "none" }}
                   ref={fileInputRef}
                   onChange={handleImageChange}
-                  multiple
                 />
               </div>
             </div>
