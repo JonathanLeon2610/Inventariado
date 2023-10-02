41207329d9d6073468f4d9d107a57043e2442574
jonathanleon2610 <jonathan2610leon@gmail.com>
2023-10-02 08:56:47 -0700

    Datos de representante

--- src/components/Proveedores/DatosRepresentante.jsx
+++ src/components/Proveedores/DatosRepresentante.jsx
@@ -1,13 +1,26 @@
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faBan } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
 
 function DatosRepresentante() {
   const [curp, setCurp] = useState("");
   const [curpError, setCurpError] = useState("");
   const url = window.location.pathname;
-    const segments = url.split("/");
-    const ultimoValor = segments[segments.length - 1];
+  const segments = url.split("/");
+  const ultimoValor = segments[segments.length - 1];
+  const [data, setData] = useState("");
+
+  const [inputValues, setInputValues] = useState({
+      id: "",
+      representanteLegal: "",
+      repCURP: "",
+      repTel: "",
+      repEmail: "",
+      actaConstitutiva: "",
+      poderNotarial: "",
+      datosNotaria: ""
+  });
 
   // Expresión regular para validar CURP en México
   const curpRegex = /^[A-Z]{4}\d{6}[HM][A-Z]{5}[A-Z0-9]\d$/;
@@ -30,14 +43,50 @@ function DatosRepresentante() {
     // Verificar si hay un error en el campo CURP
     if (!curpError) {
       // Realizar la acción de registro aquí
-      console.log("CURP válido:", curp);
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append(
+        "Authorization",
+        "Bearer " + localStorage.getItem("token")
+      );
+
+      var raw = JSON.stringify({
+        id: ultimoValor,
+        representanteLegal: inputValues.representanteLegal,
+        repCURP: inputValues.repCURP,
+        repTel: inputValues.repTel,
+        repEmail: inputValues.repEmail,
+        actaConstitutiva: inputValues.actaConstitutiva,
+        poderNotarial: inputValues.poderNotarial,
+        datosNotaria: inputValues.datosNotaria
+      });
+
+      var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      console.log(raw);
+
+      fetch(
+        `https://192.168.10.100/api/v1/proveedores/${ultimoValor}/Representante`,
+        requestOptions
+      )
+        .then((response) => response.text())
+        .then(() => {
+          Swal.fire(
+            "Registro exitoso!",
+            "Los cambios se han aplicado exitosamente!",
+            "success"
+          );
+        })
+        .catch((error) => console.log("error", error));
     } else {
       console.log("El formulario contiene errores.");
     }
-
-
   };
-
 
   useEffect(() => {
     var myHeaders = new Headers();
@@ -60,6 +109,17 @@ function DatosRepresentante() {
       .then((response) => response.json())
       .then((result) => {
         console.log(result);
+        setData(result);
+        setInputValues({
+          id: ultimoValor,
+          representanteLegal: result.representanteLegal,
+          repCURP: result.repCURP,
+          repTel: result.repTel,
+          repEmail: result.repEmail,
+          actaConstitutiva: result.actaConstitutiva,
+          poderNotarial: result.poderNotarial,
+          datosNotaria: result.datosNotaria
+        });
       })
       .catch((error) => console.log(error));
   }, []);
@@ -73,38 +133,110 @@ function DatosRepresentante() {
           <div style={{ display: "flex", flexDirection: "column" }}>
             <div>
               <label htmlFor="">Nombre:</label>
-              <input type="text" placeholder="Nombre" required />
+              <input
+                type="text"
+                placeholder="Nombre"
+                required
+                defaultValue={data.representanteLegal}
+                onChange={(e) => {
+                  setInputValues({
+                    ...inputValues,
+                    representanteLegal: e.target.value, // Convertir a mayúsculas
+                  });
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">CURP:</label>
               <input
                 type="text"
                 placeholder="CURP"
-                value={curp}
-                onChange={handleCurpChange}
-                required
+                defaultValue={data.repCURP}
+                onChange={(e) => {
+                  handleCurpChange,
+                  setInputValues({
+                    ...inputValues,
+                    repCURP: e.target.value.toUpperCase(), // Convertir a mayúsculas
+                  });
+                }}
+
               />
               {curpError && <p style={{ color: "red" }}>{curpError}</p>}
             </div>
             <div>
               <label htmlFor="">Telefono:</label>
-              <input type="text" placeholder="Telefono" required />
+              <input
+                type="text"
+                placeholder="Telefono"
+                required
+                defaultValue={data.repTel}
+                onChange={(e) => {
+                  setInputValues({
+                    ...inputValues,
+                    repTel: e.target.value, // Convertir a mayúsculas
+                  });
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">Email:</label>
-              <input type="text" placeholder="Email" required />
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                defaultValue={data.repEmail}
+                onChange={(e) => {
+                  setInputValues({
+                    ...inputValues,
+                    repEmail: e.target.value, // Convertir a mayúsculas
+                  });
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">No. de acta constitutiva:</label>
-              <input type="text" placeholder="No. de acta constitutiva" required />
+              <input
+                type="text"
+                placeholder="No. de acta constitutiva"
+                required
+                defaultValue={data.actaConstitutiva}
+                onChange={(e) => {
+                  setInputValues({
+                    ...inputValues,
+                    actaConstitutiva: e.target.value, // Convertir a mayúsculas
+                  });
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">No. Poder notarial:</label>
-              <input type="text" placeholder="No. Poder notarial" required />
+              <input
+                type="text"
+                placeholder="No. Poder notarial"
+                required
+                defaultValue={data.poderNotarial}
+                onChange={(e) => {
+                  setInputValues({
+                    ...inputValues,
+                    poderNotarial: e.target.value, // Convertir a mayúsculas
+                  });
+                }}
+              />
             </div>
             <div>
               <label htmlFor="">Datos de la notaria:</label>
-              <input type="text" placeholder="Datos de la notaria" required />
+              <input
+                type="text"
+                placeholder="Datos de la notaria"
+                required
+                defaultValue={data.datosNotaria}
+                onChange={(e) => {
+                  setInputValues({
+                    ...inputValues,
+                    datosNotaria: e.target.value, // Convertir a mayúsculas
+                  });
+                }}
+              />
             </div>
           </div>
 
